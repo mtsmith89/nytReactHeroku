a0392b7d2b7ad43b81f8808b2fc6df9847d564d0
mtsmith89 <travis.smith2@gmail.com>
2018-07-16 21:19:37 -0400

    heroku

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const articlesController = require("../../controllers/articlesController");
 
-router
-  .route("/")
+// Matches with "/api/articles"
+router.route("/")
   .get(articlesController.findAll)
   .post(articlesController.create);
 
+// Matches with "/api/articles/:id"
 router
   .route("/:id")
   .get(articlesController.findById)
